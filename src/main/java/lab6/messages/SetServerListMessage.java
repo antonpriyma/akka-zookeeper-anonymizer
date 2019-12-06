e7540763d9239c1f0258f537cf71ffe9e70f40e7
AntonPriyma <apriyma0@gmaul.com>
2019-12-07 02:32:54 +0300

    Scripted auto-commit on change (2019-12-07 02:32:54) by gitwatch.sh

--- src/main/java/lab6/messages/SetServerListMessage.java
+++ src/main/java/lab6/messages/SetServerListMessage.java
@@ -1,4 +1,14 @@
 package lab6.messages;
 
 public class SetServerListMessage {
+
+    private String[] serverList;
+
+    public SetServerListMessage(String[] serverList) {
+        this.serverList = serverList;
+    }
+
+    public String[] getServerList() {
+        return serverList;
+    }
 }
