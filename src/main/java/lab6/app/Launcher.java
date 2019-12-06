454dbb82bea45da98e8ec0f5b741b43434aeac06
AntonPriyma <apriyma0@gmaul.com>
2019-12-07 02:55:27 +0300

    Scripted auto-commit on change (2019-12-07 02:55:27) by gitwatch.sh

--- src/main/java/lab6/app/Launcher.java
+++ src/main/java/lab6/app/Launcher.java
@@ -1,12 +1,16 @@
 package lab6.app;
 
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
+import lab6.actors.ConfigStoreActor;
 
 public class Launcher {
 
-    private static final String ACTOR_SYSTEM_NAME = "anonymizer-system"
+    private static final String ACTOR_SYSTEM_NAME = "anonymizer-system";
 
     public static void main(String[] args) {
-        ActorSystem system = ActorSystem.create();
+        ActorSystem system = ActorSystem.create(ACTOR_SYSTEM_NAME);
+        ActorRef configStoreActor = system.actorOf(Props.create(ConfigStoreActor.class));
     }
 }
