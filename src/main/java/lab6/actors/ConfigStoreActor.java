858da1ce11c0a28dc0624f929a2103d0727614b3
AntonPriyma <apriyma0@gmaul.com>
2019-12-07 02:33:50 +0300

    Scripted auto-commit on change (2019-12-07 02:33:50) by gitwatch.sh

--- src/main/java/lab6/actors/ConfigStoreActor.java
+++ src/main/java/lab6/actors/ConfigStoreActor.java
@@ -2,11 +2,12 @@ package lab6.actors;
 
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
+import lab6.messages.SetServerListMessage;
 
 public class ConfigStoreActor extends AbstractActor {
 
     @Override
     public Receive createReceive() {
-        return ReceiveBuilder.create().match(); //TODO: создать классы сообщений
+        return ReceiveBuilder.create().match(SetServerListMessage.class,); //TODO: создать классы сообщений
     }
 }
