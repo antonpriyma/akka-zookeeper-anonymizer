753606bf4400f7885c861bbfa5879fbb5d1525e7
AntonPriyma <apriyma0@gmaul.com>
2019-12-07 02:54:04 +0300

    Scripted auto-commit on change (2019-12-07 02:54:04) by gitwatch.sh

--- src/main/java/lab6/app/Launcher.java
+++ src/main/java/lab6/app/Launcher.java
@@ -1,8 +1,12 @@
 package lab6.app;
 
+import akka.actor.ActorSystem;
+
 public class Launcher {
 
+    private static final String ACTOR_SYSTEM_NAME = "anonymizer-system"
+
     public static void main(String[] args) {
-        
+        ActorSystem system = ActorSystem.create();
     }
 }
