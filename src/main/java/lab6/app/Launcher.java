93e07fb7f58fe1c696cd0cb4217587842e3a490b
AntonPriyma <apriyma0@gmaul.com>
2019-12-07 02:57:02 +0300

    Scripted auto-commit on change (2019-12-07 02:57:02) by gitwatch.sh

--- src/main/java/lab6/app/Launcher.java
+++ src/main/java/lab6/app/Launcher.java
@@ -3,6 +3,8 @@ package lab6.app;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
 import lab6.actors.ConfigStoreActor;
 
 public class Launcher {
@@ -12,5 +14,9 @@ public class Launcher {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create(ACTOR_SYSTEM_NAME);
         ActorRef configStoreActor = system.actorOf(Props.create(ConfigStoreActor.class));
+
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        //TODO:create server
     }
 }
