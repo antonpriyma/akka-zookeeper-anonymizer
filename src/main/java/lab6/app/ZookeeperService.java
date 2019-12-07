b3f6295f83136318b82814bbc37460e5484af70c
AntonPriyma <apriyma0@gmaul.com>
2019-12-07 11:02:53 +0300

    Scripted auto-commit on change (2019-12-07 11:02:53) by gitwatch.sh

--- src/main/java/lab6/app/ZookeeperService.java
+++ src/main/java/lab6/app/ZookeeperService.java
@@ -5,7 +5,7 @@ import org.apache.zookeeper.ZooKeeper;
 public class ZookeeperService {
 
     private static final String ZOOKEEPER_CONNECT_STRING = "127.0.0.1:2181";
-    private static final int SESSION_TIMEOUT = 
+    private static final int SESSION_TIMEOUT = 3000;
 
     private ZooKeeper zooKeeper;
 
@@ -14,6 +14,6 @@ public class ZookeeperService {
     }
 
     private ZooKeeper createZooKeeper() {
-        return new ZooKeeper(ZOOKEEPER_CONNECT_STRING, )
+        return new ZooKeeper(ZOOKEEPER_CONNECT_STRING, SESSION_TIMEOUT)
     }
 }
