6d9555b0d2510f1c58a615eb6aaf4de44dd2b133
AntonPriyma <apriyma0@gmaul.com>
2019-12-07 10:14:24 +0300

    Scripted auto-commit on change (2019-12-07 10:14:24) by gitwatch.sh

--- src/main/java/lab6/app/Server.java
+++ src/main/java/lab6/app/Server.java
@@ -6,12 +6,13 @@ import akka.http.javadsl.server.Route;
 public class Server extends AllDirectives {
 
     private static final String URL_PARAM_NAME = "url";
+    private static final String COUNT_PARAM_NAME = "count";
 
     public Server() {
         //TODO: add constructor
     }
 
     public Route createRoute() {
-        return get(() -> parameter(URL_PARAM_NAME, url -> parameter()));
+        return get(() -> parameter(URL_PARAM_NAME, url -> parameter(COUNT_PARAM_NAME)));
     }
 }
