19e372840367e594b1b5082aa4be459b7f0cd74c
AntonPriyma <apriyma0@gmaul.com>
2019-12-07 08:28:38 +0300

    Scripted auto-commit on change (2019-12-07 08:28:38) by gitwatch.sh

--- src/main/java/lab6/app/Server.java
+++ src/main/java/lab6/app/Server.java
@@ -1,8 +1,15 @@
 package lab6.app;
 
 import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
 
 public class Server extends AllDirectives {
 
-    
+    public Server() {
+        //TODO: add constructor
+    }
+
+    public Route createRoute() {
+        return route();
+    }
 }
