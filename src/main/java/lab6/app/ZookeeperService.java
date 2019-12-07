1ab5c6455a152ee3c615310d8715c33ab3fb46d7
AntonPriyma <apriyma0@gmaul.com>
2019-12-07 11:00:21 +0300

    Scripted auto-commit on change (2019-12-07 11:00:21) by gitwatch.sh

--- src/main/java/lab6/app/ZookeeperService.java
+++ src/main/java/lab6/app/ZookeeperService.java
@@ -1,4 +1,14 @@
 package lab6.app;
 
+import org.apache.zookeeper.ZooKeeper;
+
 public class ZookeeperService {
+
+    private static final String ZOOKEEPER_PATH = ""
+
+    private ZooKeeper zooKeeper;
+
+    public ZookeeperService() {
+
+    }
 }
