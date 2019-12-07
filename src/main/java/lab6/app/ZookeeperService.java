4247fbd0ae3e540c12a1263123cb2b854daa6ac9
AntonPriyma <apriyma0@gmaul.com>
2019-12-07 11:23:58 +0300

    Scripted auto-commit on change (2019-12-07 11:23:58) by gitwatch.sh

--- src/main/java/lab6/app/ZookeeperService.java
+++ src/main/java/lab6/app/ZookeeperService.java
@@ -9,7 +9,7 @@ public class ZookeeperService {
 
     private static final String ZOOKEEPER_CONNECT_STRING = "127.0.0.1:2181";
     private static final String ROOT_PATH = "/servers";
-    private static final String NODES_PATH = ""
+    private static final String NODES_PATH = "/servers/s";
     private static final int SESSION_TIMEOUT = 3000;
 
     private ZooKeeper zooKeeper;
@@ -21,6 +21,8 @@ public class ZookeeperService {
     private ZooKeeper createZooKeeper() throws IOException {
         return new ZooKeeper(ZOOKEEPER_CONNECT_STRING, SESSION_TIMEOUT, null);
     }
+
+    private 
 
     private void watchServers() {
         List<String> servers = zooKeeper.getChildren()
