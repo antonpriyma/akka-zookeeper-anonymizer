02472f753f008e97857399ef86c2f4d0a3655b18
Anton <a.priyma@corp.mail.ru>
2019-12-14 11:45:23 +0300

    UPD

---
+++ commit.sh
@@ -0,0 +1,17 @@
+git filter-branch --env-filter '
+
+OLD_EMAIL="vlad19041999@yandex.ru"
+CORRECT_NAME="AntonPriyma"
+CORRECT_EMAIL="apriyma0@gmaul.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
